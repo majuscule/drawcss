b9d97241f9d59378205032dfc5e1841d6ce8b7be
dylan <dylan@MAJUSCULE.(none)>
2010-10-22 02:39:44 -0400

    IT WORKS

--- index.php
+++ index.php
@@ -16,35 +16,30 @@
 		var pageY = 'null';
 		var pageX = 'null';
 
-		$('#drawing').mousedown(function(e){
-
-		   var x_offset = this.offsetLeft;
-		   var y_offset = this.offsetTop;
-
-		   var x_start = e.pageX - x_offset;
-		   var y_start = e.pageY - y_offset;
-
+		   var x_offset = 'NULL';
+		   var y_offset = 'NULL';
+		   var x_start = 'NULL';
+		   var y_start = 'NULL';
 		   var x_coord, y_coord = 'NULL';
 
-		   $(document).mousemove(function(e){
+		$('#drawing').mouseup(function(e){
 			pageX = e.pageX;
 			pageY = e.pageY;
 			x_coord = e.pageX - x_offset;
 			y_coord = e.pageY - y_offset;
 			x_length = x_coord - x_start;
 			y_height = y_coord - y_start;
-	   	   }); 
-
-		   $('#drawing').mouseout(function(){
-			return;
-		   });
-
-		  $('#drawing').mouseup(function(){
 			console.log("coords:"+pageX+","+pageY);
 			console.log("starts:"+x_start + "," + y_start);
 			console.log("ends:"+x_coord + "," + y_coord);
 			context.strokeRect(x_start, y_start, x_length, y_height);
-		   });
+		});
+
+		$('#drawing').mousedown(function(e){
+		   x_offset = this.offsetLeft;
+		   y_offset = this.offsetTop;
+		   x_start = e.pageX - x_offset;
+		   y_start = e.pageY - y_offset;
 		});
 	});
     </script>
